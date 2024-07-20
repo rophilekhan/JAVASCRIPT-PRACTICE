e5e219fa00f8ac99d46554f3a57cf09e69167f5c
Rophile Ahmed Khan <rophile.khan@gmail.com>
2024-07-20 21:31:33 +0500

    revise const let etc

--- practice/app.js
+++ practice/app.js
@@ -29,3 +29,66 @@
 // console.log(userName);
 
 
+// var num1 = 10;        //assign
+// num1 = num1 + 20;     //reAssign
+// console.log(num1);
+
+
+
+// comperition method
+
+// console.log(10 > 5);
+// console.log(10 < 5);
+// console.log(10 <= 5);
+// console.log(10 <= 5);
+// console.log(10 == 10);
+// console.log(10 == 10);
+// console.log(10 == '10');
+// console.log(10 === '10');
+
+
+
+// var num1 = prompt ('Enter first number');
+// var num2 = prompt ('Enter second number');
+// console.log(num1 > num2);
+
+//   != data type check nahi kr raha hai
+// console.log(10 != 10);
+// console.log(10 != 5);
+
+//  !== data type check krege
+
+// console.log(10 !== '10');
+
+
+
+// revision
+// topic var and const
+
+// const change nahi hota or var change hojata hai
+
+// const accountId = 3245689
+// let accountEmail = "rophile.khan@gmail.com"
+// var accountpassword = "12345"
+// accountCity = "karachi"
+// let accountState;
+
+// // accountId = 2  not  allowed const change nhi hota hai
+// accountEmail = "hc@gmail.com"  // let bhi chnage hojata hai
+// accountpassword = "25648"      // var bhi chnage hojata hai
+// accountCity = "lahore"         // Direct bhi chnage hojata hai
+
+// console.log(accountId);
+
+// console.table([accountId, accountEmail, accountpassword, accountCity, accountState])
+//  // console.table ([]) is ke through ap direct sab console kara skte ho
+
+
+//  /*
+// prefer not to use var because of issue in block scope and functional scope
+// const or let use krna hai var use nahi krna
+//  */
+
+
+
+// Topic data types
