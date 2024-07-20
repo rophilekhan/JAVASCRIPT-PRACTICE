b7a59ee26d58dd7a16cb0df564073bb57c4eb471
Rophile Ahmed Khan <rophile.khan@gmail.com>
2024-07-20 20:41:18 +0500

    4 topics completed

---
+++ practice/app.js
@@ -0,0 +1,31 @@
+// var
+
+
+// var num1 = 3;
+// var num2 = 6;
+// console.log(num1 + num2);
+
+// var userName = 'rophile khan'
+// console.log(userName);
+
+
+// alert 
+
+
+// alert("hey dude how are you")
+
+// var num1 = 5;
+// var num2 = 7;
+// var result = num1 + num2;
+
+// alert(result);
+
+
+
+// prompt
+
+
+// var userName = prompt ('Enter your Name');
+// console.log(userName);
+
+
