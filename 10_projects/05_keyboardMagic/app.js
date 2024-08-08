e41a41217594180cb829c067271fb64f30566f7b
Rophile Ahmed Khan <rophile.khan@gmail.com>
2024-08-09 04:05:28 +0500

    press the key and see magic game

---
+++ 10_projects/05_keyboardMagic/app.js
@@ -0,0 +1,21 @@
+const add = document.getElementById('insert')
+
+window.addEventListener('keydown', (e) => {
+    add.innerHTML =`
+        <div class="color">
+        <table>
+  <tr>
+    <th>Key</th>
+    <th>Keycode</th>
+    <th>Code</th>
+  </tr>
+  <tr>
+    <td>${e.key === " " ? "Space": e.key}</td>
+    <td>${e.keyCode}</td>
+    <td>${e.code}</td>
+  </tr>
+  
+</table>
+</div>
+    `
+})
