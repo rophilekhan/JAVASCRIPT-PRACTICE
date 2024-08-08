f0dc5acddb7aec3a06ab61b5025f7ac9ecf68c1b
Rophile Ahmed Khan <rophile.khan@gmail.com>
2024-08-09 03:40:07 +0500

    project: Bg Color change every second with start or stop function

---
+++ 11_events/03_setInterval/app.js
@@ -0,0 +1,54 @@
+// const sayDate = function (str){
+//     console.log(str, Date.now());
+    
+// }
+// const intervalId = setInterval(sayDate, 1000, "rophile")
+
+// // const stopStr = clearInterval(intervalId)
+
+// document.querySelector('#stop').addEventListener('click', function(){
+//     clearInterval(intervalId)
+//     console.log("STOPPED");
+
+// })
+
+
+
+
+// generate random color app
+
+
+const randomColor = function(){
+    const hex = "0123456789ABCDEF"
+    let color = '#'
+    for(let i = 0; i < 6; i++){
+    color += hex[Math.floor(Math.random() * 16)]
+    }
+    return color
+  }
+  
+  console.log(randomColor());
+  
+  let intervalId;
+    const startChangingColor = function(){
+        if (!intervalId) {
+            intervalId =  setInterval(changeBgColor, 1000)
+        }
+
+       function changeBgColor(){
+        document.body.style.backgroundColor = randomColor()
+       }
+    }
+
+
+    const stopChangingColor = function(){
+        clearInterval(intervalId)
+        intervalId = null;
+    }
+    
+
+
+
+  document.querySelector("#start").addEventListener('click', startChangingColor)
+
+  document.querySelector("#stop").addEventListener('click', stopChangingColor)
